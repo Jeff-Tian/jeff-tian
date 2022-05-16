0bdddd6e803335f7dd810cecf83edf54b415e8bc
Jeff Tian <jeff.tian@outlook.com>
2022-05-16 16:26:06 +0800

    feat: delete gitbook-cli & gitbook

--- packages/gitbook/lib/plugins/__tests__/locateRootFolder.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-var locateRootFolder = require('../locateRootFolder');
-
-describe('locateRootFolder', function () {
-    it('should correctly resolve the node_modules for gitbook', function () {
-        expect([path.resolve(__dirname, '../../../'), path.resolve(__dirname, '../../../../../')]).toContain(locateRootFolder());
-    });
-});
