7d9037386a8e383743831ac94527e1770a98ed24
Jeff Tian <jeff.tian@outlook.com>
2024-03-24 17:03:35 +0800

    test(UNI-210): test token invalid

--- packages/mp/__tests__/mp.test.js
+++ packages/mp/__tests__/mp.test.js
@@ -1,9 +1,37 @@
 require('chai').should();
 
 const mp = require('..');
+const nock = require("nock");
 
-describe('fetching temp media', ()=>{
-    it('fetches temp media by media id', async () =>{
+describe('fetching temp media', () => {
+    it('should return 200 with token error when token is invalid', async () => {
+        // arrange
+        const scope = nock('https://api.weixin.qq.com');
+
+        scope.post('/cgi-bin/stable_token')
+            .reply(200, {
+                access_token: 'undefined'
+            });
+
+        scope
+            .get('/cgi-bin/media/get?access_token=undefined&media_id=1234')
+            .reply(200, {
+                errcode: 40001,
+                errmsg: 'invalid credential, access_token is invalid or not latest, could get access_token by getStableAccessToken, more details at https://mmbizurl.cn/s/JtxxFh33r rid: 65ffe934-2355113d-79ef0b31'
+            });
+
+        // act
+        const result = await mp.fetchTempMedia('1234');
+
+        // assert
+        result.status.should.be.an('number');
+        result.status.should.equal(200);
+        result.data.should.be.an('object');
+        result.data.errcode.should.equal(40001);
+        result.data.errmsg.should.equal('invalid credential, access_token is invalid or not latest, could get access_token by getStableAccessToken, more details at https://mmbizurl.cn/s/JtxxFh33r rid: 65ffe934-2355113d-79ef0b31');
+    })
+
+    it.skip('fetches temp media by media id', async () => {
         // arrange
         const mediaId = 1234;
 
@@ -11,6 +39,6 @@ describe('fetching temp media', ()=>{
         const result = await mp.fetchTempMedia(mediaId);
 
         // assert
-        result.should.be.an('string','hello')
+        result.should.be.an('string', 'hello')
     })
 })
--- packages/mp/lib/mp.js
+++ packages/mp/lib/mp.js
@@ -7,139 +7,141 @@ const FormData = require('form-data')
 const {convertToHtml} = require("./markdown");
 
 module.exports.getAccessToken = async () => {
-  console.log('getting access token with ', process.env.MP_APPID)
-  return axios.post('https://api.weixin.qq.com/cgi-bin/stable_token', {
-    grant_type: 'client_credential',
-    appid: String(process.env.MP_APPID).trim(),
-    secret: String(process.env.MP_SECRET).trim(),
-    force_refresh: false
-  })
+    console.log('getting access token with ', process.env.MP_APPID)
+    return axios.post('https://api.weixin.qq.com/cgi-bin/stable_token', {
+        grant_type: 'client_credential',
+        appid: String(process.env.MP_APPID).trim(),
+        secret: String(process.env.MP_SECRET).trim(),
+        force_refresh: false
+    })
 }
 
 module.exports.addTempImage = async (access_token) => {
-  if (!access_token) {
-    const token = await module.exports.getAccessToken();
-    access_token = token.data.access_token;
-  }
+    if (!access_token) {
+        const token = await module.exports.getAccessToken();
+        access_token = token.data.access_token;
+    }
 
-  const formData = new FormData();
-  formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
+    const formData = new FormData();
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
 
-  console.log('uploading with token ', access_token);
+    console.log('uploading with token ', access_token);
 
-  return axios.post(
-      `https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${access_token}&type=image`,
-      formData,
-      {
-        headers: {
-          ...formData.getHeaders()
+    return axios.post(
+        `https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${access_token}&type=image`,
+        formData,
+        {
+            headers: {
+                ...formData.getHeaders()
+            }
         }
-      }
-  )
+    )
 }
 
 module.exports.addPersistedImage = async (access_token) => {
-  console.log('adding image with token ', access_token);
-  if (!access_token) {
-    const token = await module.exports.getAccessToken();
-    console.log('token result = ', token.data);
-    access_token = token.data.access_token;
-  }
+    console.log('adding image with token ', access_token);
+    if (!access_token) {
+        const token = await module.exports.getAccessToken();
+        console.log('token result = ', token.data);
+        access_token = token.data.access_token;
+    }
 
-  const formData = new FormData();
-  formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
+    const formData = new FormData();
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
 
-  console.log('uploading persisted image with token ', access_token);
+    console.log('uploading persisted image with token ', access_token);
 
-  return axios.post(
-      `https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=${access_token}&type=image`,
-      formData,
-      {
-        headers: {
-          ...formData.getHeaders()
+    return axios.post(
+        `https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=${access_token}&type=image`,
+        formData,
+        {
+            headers: {
+                ...formData.getHeaders()
+            }
         }
-      }
-  )
+    )
 }
 
 module.exports.addPersistedBlogImage = async (access_token, blogId) => {
-  console.log('adding image with token ', access_token);
-  if (!access_token) {
-    const token = await module.exports.getAccessToken();
-    console.log('token result = ', token.data);
-    access_token = token.data.access_token;
-  }
+    console.log('adding image with token ', access_token);
+    if (!access_token) {
+        const token = await module.exports.getAccessToken();
+        console.log('token result = ', token.data);
+        access_token = token.data.access_token;
+    }
 
-  const formData = new FormData();
-  formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
+    const formData = new FormData();
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
 
-  console.log('uploading persisted image with token ', access_token);
+    console.log('uploading persisted image with token ', access_token);
 
-  return axios.post(
-      `https://api.weixin.qq.com/cgi-bin/media/uploadimg?access_token=${access_token}&type=image`,
-      formData,
-      {
-        headers: {
-          ...formData.getHeaders()
+    return axios.post(
+        `https://api.weixin.qq.com/cgi-bin/media/uploadimg?access_token=${access_token}&type=image`,
+        formData,
+        {
+            headers: {
+                ...formData.getHeaders()
+            }
         }
-      }
-  )
+    )
 }
 
 module.exports.addDraft = async ({title, content, content_source_url}) => {
-  const token = await module.exports.getAccessToken();
+    const token = await module.exports.getAccessToken();
 
-  console.log('token result = ', token.data);
+    console.log('token result = ', token.data);
 
-  const mediaRes = await module.exports.addPersistedImage(token.data.access_token);
+    const mediaRes = await module.exports.addPersistedImage(token.data.access_token);
 
-  console.log('mediaRes= ', mediaRes.data.media_id);
+    console.log('mediaRes= ', mediaRes.data.media_id);
 
-  return axios.post(`https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token.data.access_token}`, {
-    articles: [
-      {
-        title,
-        content,
-        content_source_url,
-        need_open_comment: 1,
-        thumb_media_id: mediaRes.data.media_id
-      }
-    ]
-  });
+    return axios.post(`https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token.data.access_token}`, {
+        articles: [
+            {
+                title,
+                content,
+                content_source_url,
+                need_open_comment: 1,
+                thumb_media_id: mediaRes.data.media_id
+            }
+        ]
+    });
 }
 
 module.exports.addDrafts = async ({title, html, markdown, content_source_url, mini_program_path}) => {
-  const token = await module.exports.getAccessToken();
+    const token = await module.exports.getAccessToken();
 
-  console.log('token result = ', token.data);
+    console.log('token result = ', token.data);
 
-  const mediaRes = await module.exports.addPersistedImage(token.data.access_token);
+    const mediaRes = await module.exports.addPersistedImage(token.data.access_token);
 
-  console.log('mediaRes= ', mediaRes.data.media_id);
+    console.log('mediaRes= ', mediaRes.data.media_id);
 
-  const miniprogramCoverImageRes = await module.exports.addPersistedBlogImage(token.data.access_token);
-  console.log('miniprogramCoverImageRes = ', miniprogramCoverImageRes.data);
+    const miniprogramCoverImageRes = await module.exports.addPersistedBlogImage(token.data.access_token);
+    console.log('miniprogramCoverImageRes = ', miniprogramCoverImageRes.data);
 
-  const content = `${convertToHtml(markdown)}<mp-miniprogram data-miniprogram-appid="wx8c777d630f2b78e3" data-miniprogram-path="${mini_program_path}" data-miniprogram-title="在小程序中查看本文" data-miniprogram-imageurl="${miniprogramCoverImageRes.data.url}"></mp-miniprogram>`;
+    const content = `${convertToHtml(markdown)}<mp-miniprogram data-miniprogram-appid="wx8c777d630f2b78e3" data-miniprogram-path="${mini_program_path}" data-miniprogram-title="在小程序中查看本文" data-miniprogram-imageurl="${miniprogramCoverImageRes.data.url}"></mp-miniprogram>`;
 
-  console.log('content sent to wechat mp: ', content);
+    console.log('content sent to wechat mp: ', content);
 
-  return await Promise.all([
-    axios.post(`https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token.data.access_token}`, {
-      articles: [
-        {
-          title,
-          author: '哈德韦',
-          content,
-          content_source_url,
-          need_open_comment: 1,
-          thumb_media_id: mediaRes.data.media_id
-        }
-      ]
-    })
-  ]);
+    return await Promise.all([
+        axios.post(`https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token.data.access_token}`, {
+            articles: [
+                {
+                    title,
+                    author: '哈德韦',
+                    content,
+                    content_source_url,
+                    need_open_comment: 1,
+                    thumb_media_id: mediaRes.data.media_id
+                }
+            ]
+        })
+    ]);
 }
 
-module.exports.fetchTempMedia = async(mediaId) => {
-  return "hello"
+module.exports.fetchTempMedia = async (mediaId) => {
+    const token = await module.exports.getAccessToken();
+
+    return await axios.get(`https://api.weixin.qq.com/cgi-bin/media/get?access_token=${token.data.access_token}&media_id=${mediaId}`);
 }
